d83012f61ca8d17163768e31b53d1995cb710b22
Parth-1011 <103803443+Parth-1011@users.noreply.github.com>
2022-07-16 17:11:17 +0530

    Create 21102031-assignment-7-output.py

---
+++ 21102031-assignment-7-output.py
@@ -0,0 +1,25 @@
+Enter size of list: 5
+Enter marks: 89
+Enter marks: 91
+Enter marks: 92
+Enter marks: 78
+Enter marks: 95
+[78, 89, 91, 92, 95]
+Enter size of array: 6
+Enter element 1
+Enter element 2
+Enter element 4
+Enter element 4
+Enter element 5
+Enter element 6
+[1, 2, 4, 4, 5, 6]
+Enter element to be searched and counted: 4
+Element is present at index 2
+Number of occurrences of element is 2
+Enter number of inputs:5
+Enter number:1
+Enter number:2
+Enter number:3
+Enter number:2
+Enter number:3
+[1, 2, 3]
